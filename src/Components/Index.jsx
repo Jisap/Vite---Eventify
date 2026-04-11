a43b7a943c524786f24a0cc60aa33f240c65fd08
Jorge Matos <jisapjisap@gmail.com>
2026-04-11 18:01:27 +0200

    feat: implement Marquee component and configure global Tailwind theme and styles

--- src/Components/Index.jsx
+++ src/Components/Index.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+
 import Hero from './Index/Hero/Hero'
+import Marquee from './Index/Marquee/Marquee'
 
 const Index = () => {
   return (
     <>
       <div className='h-[5000px]'>
         <Hero />
+        <Marquee />
       </div>
     </>
   )
--- src/Components/Index/Marquee/Marquee.jsx
+++ src/Components/Index/Marquee/Marquee.jsx
@@ -1,8 +1,64 @@
-import React from 'react'
+import { Autoplay } from "swiper/modules"
+import { Swiper, SwiperSlide } from "swiper/react"
+import "swiper/css"
+import MarqueeIcon from "../../../assets/Images/Index/Marquee/icon-marquee.svg"
+
+
 
 const Marquee = () => {
+
+  const items = [
+    "Workshop Alerts",
+    "Quick Updates",
+    "Latest Updates",
+    "New Announcements",
+    "Live Countdown",
+    "Live Notice",
+    "Event Countdown",
+    "Workshop Alerts",
+    "Quick Updates",
+    "Latest Updates",
+    "New Announcements",
+    "Live Countdown",
+    "Live Notice",
+    "Event Countdown",
+    "Workshop Alerts",
+  ]
+
   return (
-    <div>Marquee</div>
+    <>
+      <div className="w-full bg-prim text-white py-5 overflow-hidden">
+        <Swiper
+          modules={[Autoplay]}
+          slidesPerView="auto"
+          spaceBetween={40}
+          loop={true}
+          speed={3000}
+          autoplay={{
+            delay: 0,
+            disableOnInteraction: false
+          }}
+          freeMode={true}
+          className="swiper-wrapper"
+        >
+          {items.map((text, index) => (
+            <SwiperSlide key={index} className="w-auto!">
+              <div className="flex items-center">
+                <img
+                  src={MarqueeIcon}
+                  alt="marquee-icon"
+                  className="me-7"
+                />
+
+                <span className="text-3xl whitespace-nowrap hover:text-black transition-colors duration-300 cursor-pointer">
+                  {text}
+                </span>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </>
   )
 }
 
