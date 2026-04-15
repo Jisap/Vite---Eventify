24ac43508ab939fb220d2ed83b2e81b1fb47c437
Jorge Matos <jisapjisap@gmail.com>
2026-04-15 13:22:52 +0200

    feat: add Testimonials component with swiper integration and brand showcase

--- src/Components/Index/Testimonials/Testimonials.jsx
+++ src/Components/Index/Testimonials/Testimonials.jsx
@@ -3,6 +3,7 @@ import author1 from "../../../assets/Images/Index/Testimonials/author-1.jpg"
 import author2 from "../../../assets/Images/Index/Testimonials/author-2.jpg"
 import author3 from "../../../assets/Images/Index/Testimonials/author-3.jpg"
 import author4 from "../../../assets/Images/Index/Testimonials/author-4.jpg"
+import tstbg from "../../../assets/Images/bg-image.png"
 import tstbf from "../../../assets/Images/Index/Testimonials/icon-marquee.svg"
 import Mainbtn from "../../Buttons/Mainbtn"
 import { Swiper, SwiperSlide } from "swiper/react"
@@ -14,6 +15,7 @@ import brand2 from "../../../assets/Images/Index/Testimonials/company-logo-2.svg
 import brand3 from "../../../assets/Images/Index/Testimonials/company-logo-3.svg"
 import brand4 from "../../../assets/Images/Index/Testimonials/company-logo-4.svg"
 import { Link } from "react-router-dom"
+import MarqueeIcon from "../../../assets/Images/Index/About/icon-marquee.svg"
 
 
 
@@ -59,7 +61,29 @@ const Testimonials = () => {
 
   return (
     <>
-      <div>
+      <div className="features px-[2%] sm:px-[8%] lg:px-[10%] py-[6%] md:py-[10%] bg-prim-dark relative space-y-10 lg:space-y-20">
+        <div className="absolute top-0 left-0 w-full h-full">
+          <img
+            src={tstbg}
+            alt="events-image"
+            className="w-full h-full"
+          />
+        </div>
+
+        <div className="features-content text-center mx-auto max-w-full lg:max-w-180 z-1 relative">
+          <span className="flex items-center bg-prim w-fit mx-auto rounded-full text-white pe-3 text-sm md:text-md font-medium mb-3">
+            <img
+              src={marqueeIcon}
+              alt="marquee-icon"
+              className="w-7 h-7 md:w-8 md:h-8 p-2"
+            />
+            Testimonials
+          </span>
+
+          <h2 className="text-2xl sm:text-4xl md:text-5xl font-bold mb-4 text-white">
+            What our customers say about their experience
+          </h2>
+        </div>
 
       </div>
     </>
