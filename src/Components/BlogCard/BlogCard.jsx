e5a32891783dec0225329bcffe9ff0a2c0b5eb7f
Jorge Matos <jisapjisap@gmail.com>
2026-04-16 13:11:04 +0200

    feat: implement blog section with dynamic BlogCard component and data source

--- src/Components/BlogCard/BlogCard.jsx
+++ src/Components/BlogCard/BlogCard.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
+import { Icon } from "@iconify/react"
+import { desc } from "framer-motion/client"
+import { Link } from "react-router-dom"
 
-const BlogCard = () => {
+
+
+const arrow = "/Images/Index/Blogs/arrow-icon.svg"
+
+const BlogCard = ({
+  id,
+  image,
+  title,
+  author,
+  desc,
+  arrowImg,
+  className = "",
+  titleClass = "",
+  descClass = "",
+}) => {
   return (
-    <div>BlogCard</div>
+    <>
+      <Link
+        to={`/blog/${id}`}
+        className={`blog-item bg-gray-light p-3 rounded-md space-y-5 group cursor-pointer shadow-md block ${className}`}
+      >
+        <div className="blog-image rounded-md overflow-hidden">
+          <img
+            src={image}
+            alt={title}
+            className="w-full group-hover:scale-110 transition-all duration-500"
+          />
+        </div>
+
+        <div className="blog-content md:px-6">
+          <span className="flex items-center gap-2 text-gray-500 pb-3">
+            <Icon
+              icon="ri:user-line"
+              width="24"
+              height="24"
+              className="bg-prim w-6 h-6 rounded-full p-1 text-white"
+            />
+            {author}
+          </span>
+
+          <h4 className={`text-xl md:text-2xl font-semibold pb-3 ${titleClass}`}>
+            {title}
+          </h4>
+
+          <p className={`text-gray-500 text-lg pb-8 border-b border-gray-500/10 ${descClass}`}>
+            {desc}
+          </p>
+
+          <div className="flex items-center py-5 text-black font-semibold">
+            Read More
+            <img src={arrowImg} alt="arrow" className="ms-2 brightness-0" />
+          </div>
+        </div>
+      </Link>
+    </>
   )
 }
 
--- src/Components/Index/Blogs/Blogs.jsx
+++ src/Components/Index/Blogs/Blogs.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import { Icon } from "@iconify/react"
+import { Link } from "react-router-dom"
+import BlogsData from "../../../Data/Blog.json"
+import BlogCard from "../../BlogCard/BlogCard"
+
+
 
 const Blogs = () => {
+
+  const MarqueeIcon = "/Images/Index/About/icon-marquee.svg"
+  const blog2 = "/Images/Index/Blogs/blog-02.jpg"
+  const blog3 = "/Images/Index/Blogs/blog-03.jpg"
+  const arrow = "/Images/Index/Blogs/arrow-icon.svg"
+
+
+
+
   return (
-    <div>Blogs</div>
+    <>
+      <div className='blog px-[2%] sm:px-[8%] lg:px-[10%] py-[6%] md:py-[10%] relative space-y-10 lg:space-y-20'>
+        <div className='blog-conten text-center mx-auto max-w-full lg:max-w-180 z-1 relative'>
+          <span className='flex items-center bg-prim w-fit mx-auto rounded-full text-white pe-3 text-sm md:text-md font-medium mb-3'>
+            <img src={MarqueeIcon} alt="marquee-icon" className='p-2 w-7 h-7 md:w-8 md:h-8' />
+            Latest Blog
+          </span>
+
+          <h2 className='text-2xl sm:text-4xl md:text-5xl font-bold text-black'>
+            Explore our latest insights stories and updates
+          </h2>
+
+        </div>
+
+        <div className='blog-main grid grid-cols-1 xl:grid-cols-2 gap-10'>
+          {BlogsData.slice(0, 1).map((item) => (
+            <BlogCard
+              key={item.id}
+              id={item.id}
+              image={item.image}
+              title={item.title}
+              author={item.author}
+              desc={item.desc}
+              arrowImg={item.arrowImg}
+            />
+          ))}
+        </div>
+      </div>
+    </>
   )
 }
 
